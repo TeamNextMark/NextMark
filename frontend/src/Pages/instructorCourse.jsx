4ff02d602ff5cff007c493d1483d82acc040aea1
BrenPhilippe <bphilippe@atu.edu>
2026-04-07 19:38:27 -0500

    Debug Line for course name

--- frontend/src/Pages/instructorCourse.jsx
+++ frontend/src/Pages/instructorCourse.jsx
@@ -35,6 +35,7 @@ function InstructorCourse() {
         if (!assignmentsRes.ok) throw new Error("Failed to load assignments");
 
         const courseData = await courseRes.json();
+        console.log("courseData:", courseData);
         const assignmentData = await assignmentsRes.json();
 
         setCourse(courseData);
